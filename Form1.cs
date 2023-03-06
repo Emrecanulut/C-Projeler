9e5b9b135f454bb6531908eca7dbb185313f22ab
Emrecanulut <123844592+Emrecanulut@users.noreply.github.com>
2023-03-06 09:18:35 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace İngilizcekelimeProje5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)//silme butonunu eklemdim.
+        {
+            int vote = NotBook.SelectedIndex;//listbox'tan eleman seçilmesini istiyorum.
+            if (vote != -1)//burda eleman seçilip seçilmediğini kontrol ediyorum.
+            {
+                NotBook.Items.RemoveAt(vote);
+            }
+            else
+            {
+                MessageBox.Show("make a choice!");// eğer eleman seçilmemiş ise seçilmesi için uyarı veriyorum.
+            }
+
+        }
+
+        private void Ekle_Click(object sender, EventArgs e)
+        {
+            NotBook.Items.Add(textBox1.Text);//list boxa ekleme yapıyorum.
+            textBox1.Clear();//ekleme yaptıktan sonra textboxun temizliyorum.
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            NotBook.Items.Clear();// temizle butonu ekleyerek istboxun tamamının temizlenmesini sağlıyorum.
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace KDV_Ögrenci_Proje4
+//C# CONSOLE: Girilen ürün fiyatına göre ½18 kdv uygulanmış fiyatını ve ½5 öğrenci indirimi uygulanmış fiyatını ekrana yazan programın uygulamasını yapınız.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double islem,kdvlihesap = 0,ögrencihesap=0;//değişkenleri belirledim.
+            Console.Write("Fiyatı Giriniz :");// hesap yapılacak tutarı istedim.
+            double sayigir = double.Parse(Console.ReadLine());
+
+            islem = sayigir * 0.18;//kdv işlemini yaptım.
+            kdvlihesap = islem + sayigir;//girilen fiyatın üstüne ekledim.
+            ögrencihesap = sayigir -(sayigir * 0.05);//girilen fiyattan öğrenci indirimini çıkardım.
+
+            Console.WriteLine("KDV'li hesap :"+kdvlihesap);
+            Console.WriteLine("Ögrenci indirimli hesap :" + ögrencihesap);//çıktıları aldım.
+        }
+    }
+}
